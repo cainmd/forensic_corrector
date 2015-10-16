548959b5d148b92cfb9f98525ec8199a46814afb
cainmd <cainmd@uab.edu>
2015-10-16 15:39:16 -0500

    update main.js

--- js/main.js
+++ js/main.js
@@ -193,7 +193,8 @@ var interpret_report = function (report) {
 	//at start
 	extracted_value = report.substr(start + 5, 3);	
 	age = extracted_value.replace(/\s+/g, '');
-	age = age.match(/\d+/)[0];
+	alert (age);
+	//age = age.match(/\d+/)[0];
 	start = report.indexOf("SEX");
 		end_pt = report.indexOf("HEIGHT");
 	//at start
